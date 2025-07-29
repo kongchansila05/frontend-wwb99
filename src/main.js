47445da7aedf9ca7f5bcb899ba83d29b3a2b892b
kongchansila05 <chansila2222.com>
2025-07-29 10:40:16 +0700

    head simple

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './style.css'
 const app = createApp(App)
 const head = createHead()
-app.config.globalProperties.$apiBaseUrl = 'http://localhost:8080/'
-// app.config.globalProperties.$apiBaseUrl = 'https://wwb99-golang-production.up.railway.app/'
+// app.config.globalProperties.$apiBaseUrl = 'http://localhost:8080/'
+app.config.globalProperties.$apiBaseUrl = 'https://wwb99-golang-production.up.railway.app/'
 app.use(router)
 app.use(head) // <-- This line is essential!
 app.mount('#app')
